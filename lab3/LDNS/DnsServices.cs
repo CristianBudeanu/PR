a6c59a41da6344eb0ea8429b2a0b03bf3da88991
Cristian Budeanu <budeanu.cristian28@gmail.com>
2024-04-02 11:18:51 +0300

    leb3

---
+++ lab3/LDNS/DnsServices.cs
@@ -0,0 +1,110 @@
+﻿using System.Net;
+using DnsClient;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace LDNS
+{
+     public class DnsServices
+     {
+
+          private static ILookupClient _lookupClient = new LookupClient();
+
+          public static void FindIpFromDomain(string domainName)
+          {
+               try
+               {
+                    var result = _lookupClient.Query(domainName, QueryType.A);
+                    var addressRecords = result.Answers.ARecords().Select(ptr => ptr.Address).ToList();
+                    if (addressRecords.Any())
+                    {
+                         foreach (var record in result.Answers.ARecords())
+                         {
+                              Console.WriteLine($"IP: {record.Address}");
+                         }
+                    }
+                    else
+                    {
+                         Console.WriteLine("Invalid Domain");
+                    }
+               }
+               catch (Exception e)
+               {
+                    Console.WriteLine($"Err: {e.Message}");
+               }
+          }
+
+          public static void FindDomainFromIp(string ipAddress)
+          {
+               try
+               {
+                    var result = _lookupClient.QueryReverse(IPAddress.Parse(ipAddress));
+                    var domains = result.Answers.PtrRecords().Select(ptr => ptr.PtrDomainName.Value).ToList();
+
+                    if (domains.Any())
+                    {
+                         foreach (var domainName in domains)
+                         {
+                              Console.WriteLine($"Domain : {domainName}");
+                         }
+                    }
+                    else
+                    {
+                         Console.WriteLine("Invalid domain.");
+                    }
+               }
+               catch (Exception e)
+               {
+                    Console.WriteLine($"Exception : {e}");
+               }
+          }
+
+          public static string ChangeDnsServers(IPAddress dnsServer)
+          {
+               if (CheckDNS(dnsServer))
+               {
+                    var options = new LookupClientOptions(dnsServer);
+                    _lookupClient = new LookupClient(options);
+                    DisplayDnsServer();
+                    return "Serverul DNS a fost schimbat cu succes.";
+               }
+
+               DisplayDnsServer();
+               return "Nu s-a putut schimba serverul DNS. Verificați disponibilitatea serverului.";
+          }
+
+          private static bool CheckDNS(IPAddress dnsServer)
+          {
+               try
+               {
+                    using (var ping = new Ping())
+                    {
+                         var reply = ping.Send(dnsServer);
+                         if (reply.Status == IPStatus.Success)
+                         {
+                              return true;
+                         }
+                    }
+               }
+               catch (PingException)
+               {
+                    return false;
+               }
+
+               return false;
+          }
+
+
+          public static void DisplayDnsServer()
+          {
+
+               var nameServer = _lookupClient.NameServers;
+
+               foreach (var DNSServers in _lookupClient.NameServers)
+               {
+                    Console.WriteLine($"DNS Server IP: {DNSServers.Address}");
+               }
+               
+          }
+     }
+}
---
+++ lab3/LDNS/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net;
+using LDNS;
+
+class Program
+{
+     static void Main(string[] args)
+     {
+          int command = -1;
+
+          while (command != 9)
+          {
+               Console.Clear();
+               Console.WriteLine("".PadLeft(50, '='));
+               Console.WriteLine("".PadLeft(21) + "DNS Utility Menu" + "".PadRight(21));
+               Console.WriteLine("".PadLeft(50, '='));
+               Console.WriteLine("1. Find IP by DNS");
+               Console.WriteLine("2. Find DNS by IP");
+               Console.WriteLine("3. Use different DNS than system");
+               Console.WriteLine("4. Display current DNS server");
+               Console.WriteLine("9. Exit");
+               Console.WriteLine("".PadLeft(50, '='));
+               Console.WriteLine("Command: ");
+               command = int.Parse(Console.ReadLine());
+               Menu(command);
+          }
+     }
+
+     private static void Menu(int command)
+     {
+          switch (command)
+          {
+               case 1:
+                    Console.Clear();
+                    Console.WriteLine("".PadLeft(50, '='));
+                    Console.WriteLine("".PadLeft(19) + "Find IP by DNS" + "".PadRight(19));
+                    Console.WriteLine("".PadLeft(50, '='));
+                    Console.WriteLine("Enter DNS: ");
+                    var dns = Console.ReadLine();
+                    DnsServices.FindIpFromDomain(dns);
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+               case 2:
+                    Console.Clear();
+                    Console.WriteLine("".PadLeft(50, '='));
+                    Console.WriteLine("".PadLeft(18) + "Find DNS by IP" + "".PadRight(18));
+                    Console.WriteLine("".PadLeft(50, '='));
+                    Console.WriteLine("Enter IPAddress: ");
+                    var ipAddress = Console.ReadLine();
+                    DnsServices.FindDomainFromIp(ipAddress);
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+               case 3:
+                    Console.Clear();
+                    Console.WriteLine("".PadLeft(50, '='));
+                    Console.WriteLine("".PadLeft(22) + "Change DNS Server" + "".PadRight(22));
+                    Console.WriteLine("".PadLeft(50, '='));
+                    Console.WriteLine("Enter new DNS server: ");
+                    var dnsServer = Console.ReadLine();
+                    try
+                    {
+                         DnsServices.ChangeDnsServers(IPAddress.Parse(dnsServer));
+                    }
+                    catch (Exception e)
+                    {
+                         Console.WriteLine("Invalid DNS Server.");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+               case 4:
+                    Console.Clear();
+                    Console.WriteLine("".PadLeft(50, '='));
+                    Console.WriteLine("".PadLeft(20) + "Current DNS Server" + "".PadRight(20));
+                    Console.WriteLine("".PadLeft(50, '='));
+                    DnsServices.DisplayDnsServer();
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+               case 9:
+                    Environment.Exit(0);
+                    break;
+               default:
+                    Console.WriteLine("Invalid command.");
+                    break;
+          }
+     }
+}
